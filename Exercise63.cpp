78448c33ca22805cfafba866b772c0b7e14a977f
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:13:32 +0300

    Create Exercise63.cpp

---
+++ Exercise63.cpp
@@ -0,0 +1,2 @@
+//Title: Print the Pascal's Triangle for a Given Number of Rows	
+//Description: Write a program that asks the user to enter the number of rows in Pascal's Triangle and prints the triangle.
