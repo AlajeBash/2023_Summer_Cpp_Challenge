0df03d15defbef462d8376ca5f8c3b28cb2c2db5
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:36:08 +0300

    Create Exercise22.cpp

---
+++ Exercise22.cpp
@@ -0,0 +1,3 @@
+//Title: Armstrong Number	
+//Description: Write a program that asks the user to enter a number and determines whether the number is an Armstrong number or not. 
+//An Armstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
