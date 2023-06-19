da26c8526d07e547be787724f7cd2327021741ce
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:14:25 +0300

    Create Exercise64.cpp

---
+++ Exercise64.cpp
@@ -0,0 +1,3 @@
+//Title: Find the Second Largest Number in an Array	
+//Description: Write a program that asks the user to enter an array of integers and finds the second largest number in the array. 
+//Print the second largest number.
