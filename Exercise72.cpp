317e34cec73153322d6513b9f8dcba4c0602d37b
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:33:24 +0300

    Create Exercise72.cpp

---
+++ Exercise72.cpp
@@ -0,0 +1,2 @@
+//Title: Check if a Number is a Perfect Number
+//Description: Write a program that asks the user to enter a positive integer and determines whether the number is a perfect number or not.
