afaebfd7d70a4e56549ca97c0119a8c773b5a0d4
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:32:43 +0300

    Create Exercise100.cpp

---
+++ Exercise100.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Second Smallest Element in an Array using Functions
+//Description: Write a program that asks the user to enter an array of integers and finds the second smallest element in the array using a function.
