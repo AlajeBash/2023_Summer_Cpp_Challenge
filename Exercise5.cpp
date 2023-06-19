72cc3be4beda29ff0359b3b054f6a75bba8f893e
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:09:43 +0300

    Create Exercise5.cpp

---
+++ Exercise5.cpp
@@ -0,0 +1,3 @@
+//Title: Factorial
+//Description: Write a program that calculates the factorial of a given number.
+//The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.	
