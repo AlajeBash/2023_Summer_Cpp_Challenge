4a6e36a2f35582a6410822df35dc52aa8d10d787
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:14:45 +0300

    Create Exercise50.cpp

---
+++ Exercise50.cpp
@@ -0,0 +1,3 @@
+//Title: Generate a Pascal's Triangle
+//Description: Write a program that asks the user to enter the number of rows in Pascal's Triangle and generates the triangle. 
+//Print the triangle.
