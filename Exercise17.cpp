100122e85f49219342b9cb62f0e4de2c54d222a7
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:28:17 +0300

    Create Exercise17.cpp

---
+++ Exercise17.cpp
@@ -0,0 +1,3 @@
+//Title: Count Vowels and Consonants	
+//Description: Write a program that asks the user to enter a string and counts the number of vowels and consonants in the string. 
+//Ignore spaces and consider uppercase and lowercase letters as the same.
