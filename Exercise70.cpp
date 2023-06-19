0599371c99b01c6b7ebaf680fff4f52f99089bab
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:30:47 +0300

    Create Exercise70.cpp

---
+++ Exercise70.cpp
@@ -0,0 +1,2 @@
+//Title: Calculate the Factorial of a Number using Recursion
+//Description: Write a program that asks the user to enter a positive integer and calculates its factorial using recursion.
