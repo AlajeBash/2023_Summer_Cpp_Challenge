d635a4d05ca70c6224c7af04d7deafee829ef3d2
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:18:52 +0300

    Create Exercise11.cpp

---
+++ Exercise11.cpp
@@ -0,0 +1,3 @@
+//Title: Leap Year	
+//Description: Write a program that asks the user to enter a year and determines whether the year is a leap year or not. 
+//A leap year is divisible by 4 but not divisible by 100, except if it is also divisible by 400.	
