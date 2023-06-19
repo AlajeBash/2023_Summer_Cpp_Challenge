b6dcd89038c59309e0f338e568d4d41352d6da07
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:16:50 +0300

    Create Exercise84.cpp

---
+++ Exercise84.cpp
@@ -0,0 +1,2 @@
+//Title:	Check if a Number is Prime using Functions
+//Description: Write a program that asks the user to enter a positive integer and determines whether the number is prime or not using a function.
