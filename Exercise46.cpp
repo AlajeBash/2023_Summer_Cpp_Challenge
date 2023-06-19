64767b2b8199a58fc5950bbd0ef47375cc81e5fc
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:09:00 +0300

    Create Exercise46.cpp

---
+++ Exercise46.cpp
@@ -0,0 +1,3 @@
+//Title: Count the Frequency of Elements in an Array	
+//Description: Write a program that asks the user to enter an array of integers and counts the frequency of each element in the array. 
+//Print the element and its frequency.
