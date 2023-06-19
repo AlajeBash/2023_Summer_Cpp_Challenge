8c74d80c8f9b20fd00acb56620f9beca3e7b166f
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:00:37 +0300

    Create Exercise56.cpp

---
+++ Exercise56.cpp
@@ -0,0 +1,3 @@
+//Title: Check if String is a Pangram	
+//Description: Write a program that asks the user to enter a string and determines whether the string is a pangram or not. 
+//A pangram is a sentence that contains every letter of the alphabet at least once.
