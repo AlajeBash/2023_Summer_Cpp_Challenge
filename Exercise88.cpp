2445075cab4aa7071258436700e555fc2e314b19
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:21:48 +0300

    Create Exercise88.cpp

---
+++ Exercise88.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Maximum and Minimum Values in an Array using Functions	
+//Description: Write a program that asks the user to enter an array of integers and finds the maximum and minimum values in the array using functions.
