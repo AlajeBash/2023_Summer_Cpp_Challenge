5b56b57059161f581d76ab817c3eeede5584dbbe
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:11:17 +0300

    Create Exercise47.cpp

---
+++ Exercise47.cpp
@@ -0,0 +1,3 @@
+//Title: Merge Two Sorted Arrays	
+//Description: Write a program that asks the user to enter two sorted arrays and merges them into a single sorted array. 
+//Print the merged array.
