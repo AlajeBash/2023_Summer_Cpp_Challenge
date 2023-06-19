61259622300b1d034e209a187a8c2987fc3ce911
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:28:51 +0300

    Create Exercise95.cpp

---
+++ Exercise95.cpp
@@ -0,0 +1,2 @@
+//Title:	Check if a Number is a Perfect Square using Functions	
+//Description: Write a program that asks the user to enter a positive integer and determines whether the number is a perfect square or not using a function.
