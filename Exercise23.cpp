5b1f785e0271d41d240fb2b3365c455282be83f4
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:37:31 +0300

    Create Exercise23.cpp

---
+++ Exercise23.cpp
@@ -0,0 +1,2 @@
+//Title: Perfect Number	Write a program that asks the user to enter a number and determines whether the number is a perfect number or not.
+//Description: A perfect number is a positive integer that is equal to the sum of its proper divisors (excluding the number itself).
