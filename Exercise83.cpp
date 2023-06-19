943b00a1c1ac7e635f944628e70fc108b33ec1bd
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:15:57 +0300

    Create Exercise83.cpp

---
+++ Exercise83.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the LCM of Two Numbers using Functions	
+//Description: Write a program that asks the user to enter two positive integers and calculates their least common multiple (LCM) using a function.
