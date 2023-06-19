388fc26245916317b5a4cf561f263f3df87d13d6
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:02:46 +0300

    Create Exercise42.cpp

---
+++ Exercise42.cpp
@@ -0,0 +1,3 @@
+//Title: Find Maximum and Minimum in an Array
+//Description: Write a program that asks the user to enter an array of integers and finds the maximum and minimum elements in the array. 
+//Print the maximum and minimum values.
