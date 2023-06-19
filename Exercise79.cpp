a7a1d1795194c207509690d7b3de429f0538e837
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:10:45 +0300

    Create Exercise79.cpp

---
+++ Exercise79.cpp
@@ -0,0 +1,2 @@
+//Title:	Calculate the Sum of Digits in a Number using Recursion
+//Description: Write a program that asks the user to enter a positive integer and calculates the sum of its digits using recursion.
