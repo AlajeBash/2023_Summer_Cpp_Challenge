9ccdba9042774fb3ef0dcf533f3b0391f94a970e
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:56:13 +0300

    Create Exercise36.cpp

---
+++ Exercise36.cpp
@@ -0,0 +1,3 @@
+//Title: Selection Sort	
+//Description: Write a program that asks the user to enter an array of integers and sorts it using the selection sort algorithm. 
+//Print the sorted array.
