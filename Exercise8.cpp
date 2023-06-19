c531f88d07d8d92180566ab4d2a8b6d3ac6ef6a5
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:14:16 +0300

    Create Exercise8.cpp

---
+++ Exercise8.cpp
@@ -0,0 +1,3 @@
+//Title: Palindrome
+//Description: Write a program that asks the user to enter a string and then determines whether the string is a palindrome or not. 
+//A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.	
