021e76c57ef9a22c4ce1e1a4bfb3e11725c2ee44
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:32:00 +0300

    Create Exercise71.cpp

---
+++ Exercise71.cpp
@@ -0,0 +1,3 @@
+//Title: Reverse Words in a Sentence
+//Description: Write a program that asks the user to enter a sentence and reverses the order of the words in the sentence. 
+//Print the reversed sentence.
