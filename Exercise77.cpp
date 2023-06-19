603cc39c09cb8e0b6168480cd5d4994a47906ab6
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:08:50 +0300

    Create Exercise77.cpp

---
+++ Exercise77.cpp
@@ -0,0 +1,2 @@
+//Title:	Check if a Number is an Armstrong Number
+//Description: Write a program that asks the user to enter a number and determines whether the number is an Armstrong number or not.
