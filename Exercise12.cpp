e299d038e00fb9367114dff10c07e578b6117d3e
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:21:53 +0300

    Create Exercise12.cpp

---
+++ Exercise12.cpp
@@ -0,0 +1,3 @@
+//Title: Temperature Conversion	
+//Description: Write a program that converts temperature from Celsius to Fahrenheit and vice versa. 
+//Ask the user to enter the temperature in one unit and provide the converted temperature in the other unit.
