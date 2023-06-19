b5b5c066f4f78527e4d1451ecac93056ef654505
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:04:56 +0300

    Create Exercise3.cpp

---
+++ Exercise3.cpp
@@ -0,0 +1,3 @@
+//Title: Even or Odd
+//Description: Write a program that asks the user to enter a number and then determines whether the number is even or odd. 
+//Print an appropriate message to the console.
