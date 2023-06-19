f04184cd598e78a6908cda62147d5084f4610d0e
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:17:46 +0300

    Create Exercise85.cpp

---
+++ Exercise85.cpp
@@ -0,0 +1,3 @@
+//Title:	Calculate the Power of a Number using Functions	
+//Description: Write a program that asks the user to enter a base number and an exponent. 
+//Calculate the result of raising the base number to the exponent using a function.
