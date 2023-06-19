eac026aa8dd2ec53eb477bba11b7ca2fa28c69af
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:07:46 +0300

    Create Exercise45.cpp

---
+++ Exercise45.cpp
@@ -0,0 +1,3 @@
+//Title: Sum of Array Elements
+//Description: Write a program that asks the user to enter an array of integers and calculates the sum of all elements in the array. 
+//Print the sum.
