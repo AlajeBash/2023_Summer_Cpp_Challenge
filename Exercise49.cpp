781ca67378969b940f97e8a82616f6415b2f0ccd
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:13:47 +0300

    Create Exercise49.cpp

---
+++ Exercise49.cpp
@@ -0,0 +1,3 @@
+//Title: Calculate the Average of a Dynamic Array
+//Description: Write a program that asks the user to enter a dynamic array of integers and calculates the average of all elements in the array.
+//Print the average.
