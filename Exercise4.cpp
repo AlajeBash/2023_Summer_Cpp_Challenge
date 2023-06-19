12dca3d8f3712c58078a251c2d5d73cb24d12ff2
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:07:15 +0300

    Create Exercise4.cpp

---
+++ Exercise4.cpp
@@ -0,0 +1,4 @@
+//Title: Fibonacci Sequence
+//Description: Write a program that prints the Fibonacci sequence up to a given number. 
+//The Fibonacci sequence is a series of numbers in which each number is the sum of the two preceding ones. 
+//The sequence starts with 0 and 1.
