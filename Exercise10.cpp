ba51ebbc0de84f54aabf46dbad657d469e51eba2
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:17:10 +0300

    Create Exercise10.cpp

---
+++ Exercise10.cpp
@@ -0,0 +1,3 @@
+//Title: Game	Number Guessing	
+//Description: Write a program that generates a random number between 1 and 100 and asks the user to guess the number. 
+//Provide feedback to the user (e.g., "Too high" or "Too low") until they guess the correct number.	
