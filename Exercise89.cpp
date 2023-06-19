0b613b54553a00be5319617705cd9955fe0fe5c1
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:22:41 +0300

    Create Exercise89.cpp

---
+++ Exercise89.cpp
@@ -0,0 +1,2 @@
+//Title:	Sort an Array in Ascending Order using Functions
+//Description: Write a program that asks the user to enter an array of integers and sorts the array in ascending order using a function.
