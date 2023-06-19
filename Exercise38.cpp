6cec3dd7bcabeefdd963f511c39efdd647064878
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:58:26 +0300

    Create Exercise38.cpp

---
+++ Exercise38.cpp
@@ -0,0 +1,3 @@
+//Title: Palindrome Linked List	
+//Description: Write a program that asks the user to enter a linked list and determines whether it is a palindrome or not. 
+//A palindrome linked list reads the same forward and backward.
