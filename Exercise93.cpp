b5e26c973c2dea354fcfbc48e2ce272939031a33
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:26:17 +0300

    Create Exercise93.cpp

---
+++ Exercise93.cpp
@@ -0,0 +1,2 @@
+//Title:	Calculate the Sum of Even and Odd Numbers in an Array using Functions	
+//Description: Write a program that asks the user to enter an array of integers and calculates the sum of even numbers and odd numbers separately using functions.
