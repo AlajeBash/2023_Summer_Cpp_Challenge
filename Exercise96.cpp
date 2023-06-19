fc19e3402738bdf429fcdc6b63110eca2d49802b
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:29:53 +0300

    Create Exercise96.cpp

---
+++ Exercise96.cpp
@@ -0,0 +1,2 @@
+//Title:	Calculate the Factorial of a Number using Iteration	
+//Description: Write a program that asks the user to enter a positive integer and calculates its factorial using iteration (loop).
