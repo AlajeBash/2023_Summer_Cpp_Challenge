cb0ec0fe3962af51c034441512e44e634d1bb63f
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 15:06:37 +0300

    Create Exercise44.cpp

---
+++ Exercise44.cpp
@@ -0,0 +1,3 @@
+//Title: Check if Array is Sorted	
+//Description: Write a program that asks the user to enter an array of integers and determines whether the array is sorted in ascending order or not. 
+//Print a message indicating whether the array is sorted or not.
