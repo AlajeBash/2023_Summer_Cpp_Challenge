3afaf2e941a3c0c2ebe96baeebc72694988a2692
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:13:23 +0300

    Create Exercise82.cpp

---
+++ Exercise82.cpp
@@ -0,0 +1,2 @@
+//Title: Find the GCD of Two Numbers using Functions	
+//Description: Write a program that asks the user to enter two positive integers and calculates their greatest common divisor (GCD) using a function.
