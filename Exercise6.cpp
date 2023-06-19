2b8f18c03134c5116ee26eef905812d79b681e26
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:11:14 +0300

    Create Exercise6.cpp

---
+++ Exercise6.cpp
@@ -0,0 +1,3 @@
+//Title: Prime Number
+//Description: Write a program that asks the user to enter a number and then determines whether the number is prime or not.
+//A prime number is a positive integer greater than 1 that has no positive divisors other than 1 and itself.	
