e12b5cb8744c85d873d6bd54721b329d3a246650
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:27:44 +0300

    Create Exercise94.cpp

---
+++ Exercise94.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Frequency of Elements in an Array using Functions	
+//Description: Write a program that asks the user to enter an array of integers and counts the frequency of each element in the array using functions.
