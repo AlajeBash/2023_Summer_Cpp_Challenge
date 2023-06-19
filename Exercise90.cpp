ab5f0aef796843b3e6184f5495fab2d831947338
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:23:31 +0300

    Create Exercise90.cpp

---
+++ Exercise90.cpp
@@ -0,0 +1,2 @@
+//Title:	Calculate the Average of an Array using Functions	
+//Description: Write a program that asks the user to enter an array of integers and calculates the average of all elements in the array using a function.
