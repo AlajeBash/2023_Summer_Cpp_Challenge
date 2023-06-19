dff247388bd7aebe97540bfa2734547c3360f23c
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:30:52 +0300

    Create Exercise19.cpp

---
+++ Exercise19.cpp
@@ -0,0 +1,2 @@
+//Title: Power of a Number
+//Description: Write a program that asks the user to enter a base number and an exponent, and then calculates the result of raising the base number to the exponent.
