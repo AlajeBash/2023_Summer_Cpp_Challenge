55bdb5955f7d3c571ce9a63a7619970321eaebcc
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:31:51 +0300

    Create Exercise99.cpp

---
+++ Exercise99.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Largest Element in an Array using Functions
+//Description: Write a program that asks the user to enter an array of integers and finds the largest element in the array using a function.
