9a0630cc41bb678f760614275bd42ac06d07f5ae
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:57:21 +0300

    Create Exercise37.cpp

---
+++ Exercise37.cpp
@@ -0,0 +1,3 @@
+//Title: Insertion Sort	
+//Description: Write a program that asks the user to enter an array of integers and sorts it using the insertion sort algorithm. 
+//Print the sorted array.
