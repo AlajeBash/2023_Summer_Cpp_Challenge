0c484ca6b4cb3296d1da2347cf14cf74e434406d
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:19:25 +0300

    Create Exercise86.cpp

---
+++ Exercise86.cpp
@@ -0,0 +1,2 @@
+//Title:	Count the Number of Words in a Sentence using Functions	
+//Description: Write a program that asks the user to enter a sentence and counts the number of words in the sentence using a function.
