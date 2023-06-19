d9c9a95be781aeb25d9c23fd247526f7fb873330
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:53:26 +0300

    Create Exercise34.cpp

---
+++ Exercise34.cpp
@@ -0,0 +1,3 @@
+//Title: Binary Search	
+//Description: Write a program that asks the user to enter a sorted array of integers and a target value. 
+//Perform a binary search to find the target value in the array and print its index if found, or a message indicating that it was not found.
