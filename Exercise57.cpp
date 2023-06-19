fefd1d3695800b54db2f0d6c90edf2f1feb03ff5
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:01:55 +0300

    Create Exercise57.cpp

---
+++ Exercise57.cpp
@@ -0,0 +1,3 @@
+//Title: Count Words in a Sentence
+//Description: Write a program that asks the user to enter a sentence and counts the number of words in the sentence. 
+//Assume that words are separated by spaces.
