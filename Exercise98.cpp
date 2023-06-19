03d0a6ce8d6c57d4265175cf536d6958fe2848a8
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:30:56 +0300

    Create Exercise98.cpp

---
+++ Exercise98.cpp
@@ -0,0 +1,2 @@
+//Title:	Generate Prime Numbers up to a Given Limit using Functions
+//Description: Write a program that asks the user to enter a limit and generates all prime numbers up to that limit using functions.
