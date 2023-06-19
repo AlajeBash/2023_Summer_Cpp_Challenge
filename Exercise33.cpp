36a1f520de5f0a361ec9286e1b793d6791ecf027
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 14:52:06 +0300

    Create Exercise33.cpp

---
+++ Exercise33.cpp
@@ -0,0 +1,3 @@
+//Title: Linear Search	
+//Description: Write a program that asks the user to enter an array of integers and a target value. 
+//Perform a linear search to find the target value in the array and print its index if found, or a message indicating that it was not found.
