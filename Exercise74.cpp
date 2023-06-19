e73222956ad0164b8801c7945716a40823d38341
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:06:03 +0300

    Create Exercise74.cpp

---
+++ Exercise74.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Sum of Prime Numbers within a Range
+//Description: Write a program that asks the user to enter a range (start and end values) and finds the sum of all prime numbers within that range.
