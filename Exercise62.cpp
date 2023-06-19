08e34553d2862563fdfc77d48b382fca969a869d
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 16:06:32 +0300

    Create Exercise62.cpp

---
+++ Exercise62.cpp
@@ -0,0 +1,2 @@
+//Title:	Find the Longest Word in a Sentence
+//Description: Write a program that asks the user to enter a sentence and finds the longest word in the sentence. Print the longest word.
