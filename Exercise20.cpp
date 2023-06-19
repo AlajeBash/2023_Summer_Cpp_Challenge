bc52335484b0422ec77527506f70eaa98e38d3ac
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 03:32:07 +0300

    Create Exercise20.cpp

---
+++ Exercise20.cpp
@@ -0,0 +1,3 @@
+//Title: Palindrome Array	
+//Description: Write a program that asks the user to enter an array of integers and determines whether the array is a palindrome or not. 
+//An array is considered a palindrome if it reads the same forward and backward.
