21404b42857c7ce1b4ffd50ffe10601338d45e5e
Bashir Sani <82951300+AlajeBash@users.noreply.github.com>
2023-06-19 17:20:45 +0300

    Create Exercise87.cpp

---
+++ Exercise87.cpp
@@ -0,0 +1,2 @@
+//Title:	Check if a String is a Palindrome using Functions	
+//Description: Write a program that asks the user to enter a string and determines whether the string is a palindrome or not using a function.
